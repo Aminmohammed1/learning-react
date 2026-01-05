8036ce672cad6cbeef8deb4b9adb3528c9245f36
aminPersonal <amin.mohiuddin12@gmail.com>
2026-01-05 23:32:15 +0530

    mark done and undo for the todo tasks

--- STATE/src/TodoList.tsx
+++ STATE/src/TodoList.tsx
@@ -2,9 +2,9 @@ import { useState } from "react"
 
 export const TodoList = () => {
     const [todos, setTodos] = useState([
-            {"id":1, "task": "Learn React"},
-            {"id":2, "task": "Build Apps"},
-            {"id":3, "task": "Deploy"}
+            {"id":1, "task": "Learn React", "done": false},
+            {"id":2, "task": "Build Apps", "done": false},
+            {"id":3, "task": "Deploy", "done": false}
 ])
 
 const handleDelete = (id: number) => {
@@ -12,8 +12,17 @@ const handleDelete = (id: number) => {
 }
 
 const handleAddTodo = () => {
-    const newTodo = {"id":Date.now(), "task": "get payment"}
+    const newTodo = {"id":Date.now(), "task": "get payment", "done": false}
     setTodos(todos => [...todos, newTodo]) 
+}
+
+const toggleDone = (id) => {
+    setTodos(todos.map((todo) => {
+        if(todo.id === id) {
+            return {...todo, done: !todo.done}
+        }
+        return todo;
+    }))
 }
     
     return (    
@@ -21,7 +30,8 @@ const handleAddTodo = () => {
             <ul>
                 {todos.map((item) => {
                     return <div key={item.id}>
-                        <li>{item.task}</li>
+                        <li style={{textDecoration: item.done ? "line-through" : "none"}}>{item.task}</li>
+                        <button onClick={() => toggleDone(item.id)}>{item.done ? "Undo" : "Done"}</button>
                         <button onClick={() => handleDelete(item.id)}>Delete</button>
                     </div>
                 })}
